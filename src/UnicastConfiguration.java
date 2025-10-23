95aee0a94c23b3ee3c94c3a0bf7ba152dad29d64
Nmora511 <jomoraesneto@gmail.com>
2025-10-23 16:41:20 -0300

    fix: remove reader not closed warning

--- src/UnicastConfiguration.java
+++ src/UnicastConfiguration.java
@@ -2,12 +2,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 /**
  * @author Ian Marcos Gomes e Freitas
@@ -16,24 +16,30 @@ import java.util.regex.Pattern;
  *
  */
 public class UnicastConfiguration {
-    private static final Pattern PATTERN = Pattern.compile("([0-9]+) ([a-zA-Z0-9.]+) ([0-9]+)");
-    private final HashMap<Short, IPAddressAndPort> configurationMap = new HashMap<>();
 
-    public static UnicastConfiguration LoadFromFile(File file) {
+    private static final Pattern PATTERN = Pattern.compile(
+        "([0-9]+) ([a-zA-Z0-9.]+) ([0-9]+)"
+    );
+    private final HashMap<Short, IPAddressAndPort> configurationMap =
+        new HashMap<>();
+
+    public static UnicastConfiguration LoadFromFile(File file)
+        throws FileNotFoundException, IOException, UnknownHostException {
         UnicastConfiguration configuration = new UnicastConfiguration();
 
         BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        reader = new BufferedReader(new FileReader(file));
 
-        for (String line: reader.lines().toList()) {
+        for (String line : reader.lines().toList()) {
             Matcher matcher = PATTERN.matcher(line);
 
-            if(!matcher.matches()) {
-                throw new RuntimeException("Line does not follow format of <id> <host> <port>:\n\"%s\"".formatted(line));
+            if (!matcher.matches()) {
+                reader.close();
+                throw new RuntimeException(
+                    "Line does not follow format of <id> <host> <port>:\n\"%s\"".formatted(
+                        line
+                    )
+                );
             }
 
             short ucsapId = (short) Integer.parseInt(matcher.group(1));
@@ -41,19 +47,24 @@ public class UnicastConfiguration {
             short port = (short) Integer.parseInt(matcher.group(3));
 
             if (port <= 1024) {
-                throw new RuntimeException("Port less than 1024 found in file configuration");
+                reader.close();
+                throw new RuntimeException(
+                    "Port less than 1024 found in file configuration"
+                );
             }
 
             InetAddress address = null;
-            try {
-                address = InetAddress.getByName(address_str);
-            } catch (UnknownHostException e) {
-                throw new RuntimeException("Address not found: %s".formatted(address_str));
-            }
-            IPAddressAndPort address_and_port = new IPAddressAndPort(address, port);
+            address = InetAddress.getByName(address_str);
+
+            IPAddressAndPort address_and_port = new IPAddressAndPort(
+                address,
+                port
+            );
 
             configuration.configurationMap.put(ucsapId, address_and_port);
         }
+
+        reader.close();
 
         return configuration;
     }
@@ -61,11 +72,14 @@ public class UnicastConfiguration {
     public IPAddressAndPort GetAddress(short ucsapId) {
         return configurationMap.get(ucsapId);
     }
+
     public short GetId(IPAddressAndPort address_and_port) {
-        return configurationMap.entrySet().stream()
-                .filter(entry -> entry.getValue().equals(address_and_port))
-                .findFirst()
-                .orElseThrow()
-                .getKey();
+        return configurationMap
+            .entrySet()
+            .stream()
+            .filter(entry -> entry.getValue().equals(address_and_port))
+            .findFirst()
+            .orElseThrow()
+            .getKey();
     }
 }
