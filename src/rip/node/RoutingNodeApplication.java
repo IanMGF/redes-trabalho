d6a75d70f5296a168c5688aa2dd7f881f3ec8a9c
IanMGF <ianmarcosgomesefreitas@gmail.com>
2025-12-01 22:06:53 -0300

    feat: create actual RoutingNodeApplication

--- src/rip/node/RoutingNodeApplication.java
+++ src/rip/node/RoutingNodeApplication.java
@@ -1,16 +1,58 @@
 package rip.node;
 
+import rip.manager.RoutingManagementApplication;
+import rip.manager.RoutingProtocolManagementInterface;
+import rip.manager.RoutingProtocolManagementServiceUserInterface;
+
 import java.util.Scanner;
 
 public class RoutingNodeApplication {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int port;
+        int nodeId;
+        int timeoutMilliseconds;
 
-        RoutingInformationProtocol a = new RoutingInformationProtocol((short) 1, (short) 1025);
-        RoutingInformationProtocol b = new RoutingInformationProtocol((short) 2, (short) 1026);
-        RoutingInformationProtocol c = new RoutingInformationProtocol((short) 3, (short) 1027);
-        RoutingInformationProtocol d = new RoutingInformationProtocol((short) 4, (short) 1028);
-        RoutingInformationProtocol e = new RoutingInformationProtocol((short) 5, (short) 1029);
-        RoutingInformationProtocol f = new RoutingInformationProtocol((short) 6, (short) 1030);
+        System.out.println("Iniciando aplicação da entidade nó...");
+        System.out.print("Escolha um valor para o timeout da propagação periódica, em milissegundos (default=10000): ");
+
+        String input = sc.nextLine();
+
+        if (input.isEmpty()) {
+            timeoutMilliseconds = 10000;
+        } else {
+            try {
+                timeoutMilliseconds = Integer.parseInt(input);
+            }  catch (NumberFormatException e) {
+                System.err.println("Erro: o valor em milissegundos do timeout deve ser um inteiro");
+                return;
+            }
+        }
+
+        if (timeoutMilliseconds <= 0) {
+            System.err.println("Erro: o valor em milissegundos do timeout deve ser um inteiro maior que zero");
+            return;
+        }
+
+        System.out.print("Escolha o ID do nó: ");
+        input = sc.nextLine();
+        try {
+            nodeId = Integer.parseInt(input);
+        }  catch (NumberFormatException e) {
+            System.err.println("Erro: o valor da porta deve ser um inteiro");
+            return;
+        }
+
+        System.out.print("Escolha um valor para a porta utilizada: ");
+        input = sc.nextLine();
+        try {
+            port = Integer.parseInt(input);
+        }  catch (NumberFormatException e) {
+            System.err.println("Erro: o valor da porta deve ser um inteiro");
+            return;
+        }
+
+        RoutingInformationProtocol nodeEntityProtocol = new RoutingInformationProtocol((short) nodeId, (short) port);
+        System.out.println("Aplicação da entidade nó iniciada");
     }
 }
